ac62884cf3c359b24e9452428d17726543c66ed5
gunnergeri <z.geri49@gmail.com>
2020-12-15 20:05:29 +0100

    harm

--- bead_C5DLQT/bead_C5DLQT/Form1.cs
+++ bead_C5DLQT/bead_C5DLQT/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.Entity;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace bead_C5DLQT
         {
             InitializeComponent();
             context.Hypeshops.Load();
-            dataGridView1.DataSource = context.Hypeshops.Local;
+            
             listBox1.DataSource = context.Hypeshops.Local;
             gettorles();
             listBox1.ValueMember = "Id";
@@ -29,22 +30,9 @@ namespace bead_C5DLQT
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var d = Convert.ToInt32(listBox1.SelectedValue);
-            var z = (from x in context.Hypeshops
-                     where x.Id ==  d
-                     select x).FirstOrDefault();
-            context.Hypeshops.Remove(z);
-            context.SaveChanges();
-            dataGridView1.Refresh();
-            listBox1.Refresh();
-        }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            gettorles();
-        }
+
+       
 
         private void gettorles()
         {
@@ -54,10 +42,55 @@ namespace bead_C5DLQT
             listBox1.DisplayMember = "Modell";
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            gettorles();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var d = Convert.ToInt32(listBox1.SelectedValue);
+            var z = (from x in context.Hypeshops
+                     where x.Id == d
+                     select x).FirstOrDefault();
+            context.Hypeshops.Remove(z);
+            context.SaveChanges();
+            listBox1.Refresh();
+            gettorles();
+        }
+
         private void button2_Click_1(object sender, EventArgs e)
         {
             Form2 f2 = new Form2();
             f2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "csv";
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using(StreamWriter sw=new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach(var s in context.Hypeshops)
+                {
+                    sw.Write(s.Modell);
+                    sw.Write(";");
+                    sw.Write(s.Vásárlási_ár);
+                    sw.Write(";");
+                    sw.Write(s.Resell_Ár);
+                    sw.Write(";");
+                    sw.Write(s.Méret);
+                    sw.Write(";");
+                    sw.Write(s.Megjelenés_év_);
+                    sw.Write(";");
+                    sw.WriteLine();
+                }
+
+                
+            }
         }
     }
 }
